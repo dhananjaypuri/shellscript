eaaa2ac2222d9362ce918ae208b10da79f4d4a30
dhananjaypuri <dhananjay.puri@gmail.com>
2023-06-05 15:18:23 +0000

    Added files

---
+++ setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+##hello-world-0.0.1-SNAPSHOT.war
+## Check if the user is root user ####
+
+function install_pkg {
+    pkgName=${1}
+    echo "Installing ${pkgName}";
+    if apt install ${pkgName} -y > /dev/null
+    then
+        echo "${pkgName} installed Successfully"
+        
+    else
+        echo "${pkgName} Failed to install Successfully"
+        exit 1
+    fi
+}
+
+function check_target_folder {
+    if [[ -d target/ ]]
+    then
+        echo "Deleting target directory as its already present !!!!! "
+        rm -rf target > /dev/null 
+    fi
+}
+
+function execute_mvn_command {
+    command=${1}
+    echo "Executing mvn ${command}"
+    if mvn ${command} > /dev/null
+    then
+        echo "Test Completed Successfully !!!!!"
+    else
+        echo "Test failed !!!!!"
+        exit 1
+    fi
+}
+
+function copy_artifact {
+    if cp -rvf target/hello-world-0.0.1-SNAPSHOT.war /var/lib/tomcat9/webapps/app.war
+    then
+        echo "Artifact Copied successfully !!!! "
+        echo "Try accessing app at http://54.160.255.232:8080/app"
+    else
+        echo "Error Artifact cannot be copied !!!!"
+    fi
+}
+
+if [[ ${UID} -eq 0 ]]
+then
+    echo "############ Welcome Root ############"
+    if ping -c 1 8.8.8.8 > /dev/null
+    then
+        echo "Internet is working"
+        apt update -y > /dev/null
+        check_target_folder
+        install_pkg maven
+        execute_mvn_command test
+        execute_mvn_command package
+        install_pkg tomcat9
+        copy_artifact
+    else
+        echo "Internet is not working"
+    fi
+else
+    echo "Sorry only root user is allowed to run the script"
+fi
